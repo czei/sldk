888c8642cac7b9d753e4ba768c0dd7426d6c9fb0
czei <9g0jqglotahgp7741rr0q07p3s2ly1@bots.bitbucket.org>
2024-01-25 21:24:38 -0500

    Deleted CIRCUITPY.local to prepare for github updating OTA functionality.

--- CIRCUITPY.local/secrets.py
+++
@@ -1,9 +0,0 @@
-secrets = {
-# 'ssid' : 'FBI Surveillance Van 112',
-# 'password' : '9196190607',
-'ssid' : 'Scooby',
-'password' : 'Myst3ryv@n',
-'aio_username' : 'czei',
-'aio_key' : '171a088efad0415fb800c9cd7403ed30',
-}
-
